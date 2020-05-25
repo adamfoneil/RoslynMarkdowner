b72bb04d4826007974c9a13fe2eef97a73416e3c
Adam ONeil <adamosoftware@gmail.com>
2020-05-25 19:51:10 -0400

    test working with local config

--- AppTests/BuildTests.cs
+++ AppTests/BuildTests.cs
@@ -1,4 +1,5 @@
-﻿using JsonSettings;
+﻿using AppTests.Extensions;
+using JsonSettings;
 using LibGit2Sharp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RoslynDoc.Library.Models;
@@ -12,12 +13,18 @@ namespace AppTests
         [TestMethod]
         public void BuildDapperCXContent()
         {
+            var config = Config.GetConfig();
+
             var wb = new WikiBuilder();
             var si = JsonFile.Load<SolutionInfo>(@"C:\Users\Adam\AppData\Local\RoslynMarkdowner\Dapper.CX.json");
             var helper = new CSharpMarkdownHelper() { OnlinePath = si.SourceFileBase() };
             wb.Build(@"C:\Users\Adam\Source\Repos\Dapper.CX", si, helper,
-                new UsernamePasswordCredentials() { Username = "adamosoftware@gmail.com" }, 
-                new Identity("Adam O'Neil", "adamosoftware@gmail.com"));
+                new UsernamePasswordCredentials() 
+                { 
+                    Username = config["UserName"],
+                    Password = config["Password"]
+                }, 
+                new Identity(config["DisplayName"], config["UserName"]));
         }
     }
 }
---
+++ AppTests/Static/Config.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace AppTests.Extensions
+{
+    internal static class Config
+    {
+        internal static IConfigurationRoot GetConfig()
+        {            
+            return new ConfigurationBuilder()
+                .AddJsonFile("Config/config.json")
+                .Build();            
+        }
+    }
+}
