27da90d62e136c051f6397d7492a76a99cbc8aab
Adam O'Neil <adamosoftware@gmail.com>
2022-12-09 07:20:04 -0500

    fixed issue with missing msbuild instances

--- RoslynMarkdowner.WPF/Services/MsBuildService.cs
+++ RoslynMarkdowner.WPF/Services/MsBuildService.cs
@@ -7,9 +7,8 @@ namespace RoslynMarkdowner.WPF.Services
     public class MsBuildService
     {
         public Dictionary<string, VisualStudioInstance> GetInstances()
-            => MSBuildLocator.QueryVisualStudioInstances()
-                .Select((instance, index) =>
-                    new KeyValuePair<string, VisualStudioInstance>($"{instance.Name} - {instance.Version}", instance))
-                .ToDictionary(kp => kp.Key, kp => kp.Value);
+            => MSBuildLocator.QueryVisualStudioInstances().Select((instance, index) =>
+                new KeyValuePair<string, VisualStudioInstance>($"{instance.Name} - {instance.Version}", instance))
+            .ToDictionary(kp => kp.Key, kp => kp.Value);
     }
 }
